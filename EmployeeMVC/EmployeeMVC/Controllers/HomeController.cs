d9818f682a4b2a1fe43c54ae1afa37b302018b66
Enes Kızılcin <nazifenes.kizilcin@gmail.com>
2021-10-08 14:34:33 +0300

    home controller added and now the program says 'Merhaba' by controller

---
+++ EmployeeMVC/EmployeeMVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Merhaba";
+        }
+
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
