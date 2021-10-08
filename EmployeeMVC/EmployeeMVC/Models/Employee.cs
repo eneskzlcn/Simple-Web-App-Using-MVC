41a46f781b7d8477f503fc139909e8706d6485cd
Enes Kızılcin <nazifenes.kizilcin@gmail.com>
2021-10-08 14:40:52 +0300

    Employee model created simply

---
+++ EmployeeMVC/EmployeeMVC/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson2App1.Models
+{
+    public class Employee
+    {
+
+        public int employeeId { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+    }
+}
